12cf3ee2817a7616ad572d41fd0c748111dae268
michellemej22596 <mej22596@uvg.edu.gt>
2024-05-31 00:12:37 -0600

    Feat: Scratch Projects

---
+++ portafolio-personal/src/components/Projects/ReactProjects.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+    const ReactProjects = () => (
+      <div>
+        <h2>Proyectos de Scratch</h2>
+        <iframe src="https://scratch.mit.edu/projects/1020585103/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe> 
+      </div>
+    );
+
+    export default ReactProjects;
