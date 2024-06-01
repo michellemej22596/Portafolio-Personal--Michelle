1e6794d11aca81690d3d1101df7dc11efed1b11d
michellemej22596 <mej22596@uvg.edu.gt>
2024-05-31 20:34:27 -0600

    Feat: Experience page added

--- portafolio-personal/src/components/Experience.jsx
+++ portafolio-personal/src/components/Experience.jsx
@@ -4,20 +4,48 @@ import './Experience.css';
 
 const experiences = [
   {
-    startDate: 'Enero 2020',
-    endDate: 'Diciembre 2021',
-    company: 'Empresa A',
-    position: 'Desarrollador Frontend',
-    description: 'Desarrollé aplicaciones web utilizando React y Redux. Implementé interfaces de usuario accesibles y responsivas.',
+    startDate: 'Mayo 2024',
+    endDate: 'Actualidad',
+    company: 'Plataforma Kodland',
+    position: 'Tutora de Programación',
+    description: 'Encargada de brindar la formación del curso Scratch para niños y niñas de 8 a 12 años; supervisar sus tareas, evaluar el aprendizaje y responder consultas por medio de clases virtuales. ',
   },
   {
-    startDate: 'Marzo 2018',
-    endDate: 'Diciembre 2019',
-    company: 'Empresa B',
-    position: 'Desarrollador Backend',
-    description: 'Trabajé en el desarrollo de APIs RESTful utilizando Node.js y Express. Mejoré el rendimiento de la base de datos.',
+    startDate: 'Enero 2024',
+    endDate: 'Actualidad',
+    company: 'Universidad del Valle de Guatemala',
+    position: 'Auxiliar de Cálculo I',
+    description: 'Encargada de evaluar trabajos, calificar tareas, asistir al catedrático encargado y proporcionar atención a los estudiantes del curso. ',
   },
-  // Añade más experiencias según sea necesario
+  {
+    startDate: 'Diciembre 2023',
+    endDate: 'Febrero 2024',
+    company: 'Telus International - Apple Support',
+    position: 'Asesor de Soporte Técnico',
+    description: 'Encargada de asistir a usuarios de IOS provenientes de la región de Europa con inquietudes o problemas técnicos relacionados con productos Apple.',
+  },
+  {
+    startDate: 'Nov - Dic 2020 & ',
+    endDate: 'Oct - Dic 2021',
+    company: 'Alorica LATAM - Walmart.com',
+    position: 'Agente de Servicio al Cliente (Vacacionista)',
+    description: 'Encargada de brindar la mejor atención y servicio a clientes de la plataforma Walmart.com (Estados Unidos). Realizaba procesos de reembolso, reposición, seguimiento de paquetería y más. ',
+  },
+
+];
+
+const additionalExperiences = [
+  {
+    name: 'Luis Arana (UVG)',
+    position: 'Jefe inmediato, 2024',
+    phone: '4704 1234'
+  },
+  {
+    name: 'Alejandro Rosal (Telus)',
+    position: 'Supervisor, 2024',
+    phone: '5122 2284'
+  },
+
 ];
 
 const Experience = () => (
@@ -39,6 +67,22 @@ const Experience = () => (
         </div>
       </div>
     ))}
+ <h2>Referencias Laborales</h2>
+    <div className="additional-experiences">
+      {additionalExperiences.map((exp, index) => (
+        <div key={index} className="additional-experience-item">
+          <div className="additional-experience-name">
+            {exp.name}
+          </div>
+          <div className="additional-experience-position">
+            {exp.position}
+          </div>
+          <div className="additional-experience-phone">
+            {exp.phone}
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
